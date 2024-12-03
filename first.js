22b1e9de61d247b9d3ae39c39328cc40796bde33
KurshedKhan <kurshedkhanbhadra@gmail.com>
2024-12-03 10:19:01 +0530

    JavaScript IntroPart

---
+++ first.js
@@ -0,0 +1,37 @@
+let name = "Mohan"; // variable declaration
+console.log("Name : ",name);
+
+let roll = 101;
+console.log(roll);
+
+var schoolName = "Tech Innovation";
+console.log(schoolName);
+
+var schoolName = "Shree Shyam Sr. School";
+console.log(schoolName);
+console.log(typeof schoolName);
+
+name = "Rohan";
+console.log(name);
+
+
+const coachingName = "Tech Inno";
+console.log(coachingName);
+console.log(typeof coachingName);
+
+let yes = false;
+console.log(yes);
+console.log(typeof yes);
+
+let no = true;
+console.log(no);
+console.log(typeof no);
+
+let firstName;
+console.log(firstName);
+
+firstName = null;
+console.log(firstName);
+console.log(typeof firstName);
+
+
---
+++ intro.js
@@ -0,0 +1,29 @@
+// keyword ==> let,var,const
+
+// create a variable using var keyword.
+var rollName = 101;
+console.log(rollName);
+console.log("This is my first message on console");
+console.log('Variable of roll number : ',rollName);
+
+// redeclaration & reassign a variable with var keyword.
+var rollName = 102;
+console.log(rollName);
+
+//----------------------------------------------
+var rollNumber; // variable defined
+rollNumber = 103; // assignment // initialization
+var rollNumber = 103 // variable declaration and initialization
+
+
+//----------------------------------------------
+let Name = "Gautam"; // string ==> "" & ''
+console.log(Name);
+Name = "Mohan";
+console.log(Name);
+
+//--------------------------------------
+const Coaching = "Tech Innovation";
+console.log(Coaching);
+
+
