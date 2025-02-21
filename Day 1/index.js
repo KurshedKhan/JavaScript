749f9e68dab28e47d2d0866bc1ec640ffb7c9827
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-02-21 12:22:24 +0530

    Javascript Classes

---
+++ Day 1/index.js
@@ -0,0 +1,32 @@
+// Keyword
+// let , var , const exp :=> let age = 21;
+// Keywords are reserved words in JavaScript that have special meanings.
+
+// Identifiers
+// varable Name, Function Name, Array Name, Object Name exp :=> let age = 21;
+
+
+// Variable 
+// Store data or hold values for further uses
+// exp : let age = 21;
+
+let age ; // variable Declaration
+age = 21; // variable initialization
+age = 22; // Reassign 
+// = Assignment Operator
+
+/// variable print/ variable display
+console.log(age);
+
+let num = 101;
+let num1 = 102;
+
+console.log(num,num1);
+
+const Name = "Naresh";
+console.log(Name);
+// Name = "mohan";
+// console.log(Name);
+
+ let Height = 5.6;
+console.log(Height)
