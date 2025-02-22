d670e4dcce4d18031d2c249bed24499d9c239272
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-02-22 11:26:10 +0530

    JavaScript Classes

---
+++ Day 2/index.js
@@ -0,0 +1,29 @@
+let firstName = "mohan";
+let secondName = "Sohan";
+
+var roll = 101;
+let Roll = 101;
+
+const COACHING = "Tech Innovation";
+// coaching = "mohan";
+let age = 21;
+
+let myPrice = 234;
+console.log(myPrice);
+
+const PI = 3.14;
+console.log(PI);
+
+let var1 = 10;
+
+var Name = "riya";
+var Name = "mohan";
+Name = "sohan";
+console.log(Name);
+
+
+let name  = "riya";
+name = "sohan";
+
+// const age1 = 21;
+// age1 = 22;
