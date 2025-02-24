13675218e0287cae1daf513b6a750ae86737e1ac
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-02-24 11:26:18 +0530

    JavaScript Notes

---
+++ Day 3/index.js
@@ -0,0 +1,33 @@
+
+let roll = 101 // number data type
+let isodd = true // boolean data type
+let result ; // undefined data type 
+console.log(result)
+let age = null; // null data type
+
+
+let name = "mohan" // String data type
+console.log(name);
+name[0] = "s";
+console.log(name);
+
+let num1 = 101;
+let num2 = 101.1;
+console.log(typeof num2);
+
+let str1 = 'riya';
+console.log(str1);
+let str2 = "priya";
+console.log(str2)
+let num3 = "101";
+console.log(typeof num3,num2 + 102);
+num2 + 103;
+console.log(num2)
+
+let str3 = "sohan";
+let age3 = 21;
+console.log(`My "Name" is : ${str3} and my age is ${age3}.`);// template string
+// console.log("My "Name" is",str3,"and My age is ",age3)
+console.log(typeof(isodd))
+
+// document.write(age3)
