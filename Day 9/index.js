c86071e00f714ef33a084478ef43bd934009f9f0
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-03-06 11:44:49 +0530

    JavaScript Notes

---
+++ Day 9/index.js
@@ -0,0 +1,35 @@
+let names = ["mohan","sohan","rohan","ram","Shayam"];
+console.log(names);
+
+let names1 = new Array(2);
+console.log(names1);
+
+// console.log(names[0]);
+
+let names2 = []; /// empty array
+console.log(names2);
+
+let names3 = Array.of("mohan","sohan","rinku");
+console.log(names3);
+
+console.log(Array.of("mohan","sohan"))
+
+
+console.log(names[0]) // starting element
+console.log(names.length); // len of array 
+
+console.log(names[4]); // badddddd
+console.log(names[names.length-1]); //gooooddddd
+
+names[3] = "Sita";
+names[names.length-1] = "Riya";
+console.log(names)
+
+names[0] = true;
+console.log(names);
+
+names2[0] = "Rinku";
+console.log(names2);
+
+names1[2] = "suresh";
+console.log(names1);
