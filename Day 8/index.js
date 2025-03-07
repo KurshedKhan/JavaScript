065faaf87a676e8a4220b0533bcc073e75b7c49d
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-03-07 11:33:56 +0530

    JavaScript Classes

---
+++ Day 8/index.js
@@ -0,0 +1,9 @@
+for(let i = 0 ; i <= 10 ; i++){
+  console.log("Hello World",i);
+}
+
+let homeList = ["roti","Sakar","dal","vegitable","sabji","khira"];
+
+for(let i = 0 ; i < homeList.length ; i++){
+  console.log(homeList[i]);
+}
--- Day 9/index.js
+++ Day 9/index.js
@@ -1,35 +1,56 @@
+// let names = ["mohan","sohan","rohan","ram","Shayam"];
+// console.log(names);
+
+// let names1 = new Array(2);
+// console.log(names1);
+
+// // console.log(names[0]);
+
+// let names2 = []; /// empty array
+// console.log(names2);
+
+// let names3 = Array.of("mohan","sohan","rinku");
+// console.log(names3);
+
+// console.log(Array.of("mohan","sohan"))
+
+
+// console.log(names[0]) // starting element
+// console.log(names.length); // len of array 
+
+// console.log(names[4]); // badddddd
+// console.log(names[names.length-1]); //gooooddddd
+
+// names[3] = "Sita";
+// names[names.length-1] = "Riya";
+// console.log(names)
+
+// names[0] = true;
+// console.log(names);
+
+// names2[0] = "Rinku";
+// console.log(names2);
+
+// names1[2] = "suresh";
+// console.log(names1);
+
+
 let names = ["mohan","sohan","rohan","ram","Shayam"];
+names.push("Naresh");
 console.log(names);
-
-let names1 = new Array(2);
-console.log(names1);
-
-// console.log(names[0]);
-
-let names2 = []; /// empty array
-console.log(names2);
-
-let names3 = Array.of("mohan","sohan","rinku");
-console.log(names3);
-
-console.log(Array.of("mohan","sohan"))
-
-
-console.log(names[0]) // starting element
-console.log(names.length); // len of array 
-
-console.log(names[4]); // badddddd
-console.log(names[names.length-1]); //gooooddddd
-
-names[3] = "Sita";
-names[names.length-1] = "Riya";
+console.log(names.length)
+console.log(names.push("suresh","Ramesh","Naresh"));
 console.log(names)
-
-names[0] = true;
+console.log(names.pop())
 console.log(names);
 
-names2[0] = "Rinku";
-console.log(names2);
+let homelist = [];
+console.log(homelist.pop());
+console.log(homelist);
 
-names1[2] = "suresh";
-console.log(names1);
+names.unshift("komal");
+console.log(names);
+console.log(names.unshift());
+
+names.shift();
+console.log(names);
