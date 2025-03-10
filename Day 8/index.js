f9a4b7924cb9b68a855447071466d1509424be42
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-03-10 11:42:24 +0530

    JavasScript Classes

--- Day 8/index.js
+++ Day 8/index.js
@@ -1,9 +1,34 @@
-for(let i = 0 ; i <= 10 ; i++){
-  console.log("Hello World",i);
-}
+// for(let i = 0 ; i <= 10 ; i++){
+//   console.log("Hello World",i);
+// }
 
-let homeList = ["roti","Sakar","dal","vegitable","sabji","khira"];
+// let homeList = ["roti","Sakar","dal","vegitable","sabji","khira"];
 
-for(let i = 0 ; i < homeList.length ; i++){
-  console.log(homeList[i]);
+// for(let i = 0 ; i < homeList.length ; i++){
+
+//   console.log(homeList[i]);
+
+// }
+// let tableNumber = parseInt(prompt("Enter a number : "));
+
+// for(let i = 1 ; i <= 10 ; i++){
+//   console.log(`Table of ${tableNumber} = ${tableNumber} * ${i} ==> ${tableNumber*i}`);
+// }
+
+
+// let str1 = ['JavaScript','Python','Java']
+
+// for(let i = 0 ; i < str1.length ; i++){
+//    if(java){
+         
+//    }
+// }
+
+for(let i = 1 ; i < 5 ; i++){
+  
+  for(let j = 0 ; j < i ; j++){
+    document.write("*");
+  }
+
+  document.writeln("<br>");
 }
