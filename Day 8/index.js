55682bda281f7f25bef5cc729732a8c15090c3e5
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-03-18 10:41:13 +0530

    Javascript Classes

--- Day 8/index.js
+++ Day 8/index.js
@@ -33,8 +33,78 @@
 //   document.writeln("<br>");
 // }
 
-let total = 0;
-for(let i = 1 ; i <= 50 ; i++){
-  total = total + i
+// let total = 0;
+// for(let i = 1 ; i <= 50 ; i++){
+//   total = total + i
+// }
+// console.log(total)
+
+// let i = 0
+// while(i < 5){
+//   let inputText = String(prompt("Enter you prompt for searching..."));
+//   console.log(inputText);
+// }
+
+// let i = 1 ;
+// while(i <= 10){
+//   console.log(i , "Hello World!!!");
+//   i++;
+// }
+
+// let i = 0 ;
+
+// do{
+
+//   console.log('Hello World');
+//   i++;
+// }while(i < 5);
+
+// const fruits = ["Apple", "Banana", "Grape"];
+// const names = "Kuldeep Kumar";
+// for(let  i = 0 ; i < fruits.length ; i++){
+//   console.log(fruits[i]);
+// }
+
+// for(let i of names){
+//   console.log(i)
+// }
+
+// for(let  i = 0 ; i < names.length ; i++){
+//   console.log(names[i]);
+// }
+
+
+// const name = ["mohan","Sohan","Rohan","Riya",101,true,undefined];
+
+// for(let i of name){
+//   console.log(i);
+// }
+
+
+// // use with array for in
+// for(let i in name){
+//  console.log(name[i])
+// }
+
+
+// // use with string for in
+
+// let myName = "Kuldeep Kumar";
+
+// for(let i of myName ){
+//   console.log(i);
+// }
+
+// for(let i = 0 ; i < 5 ; i++){
+//   if(i===3){
+//     break;
+//   }
+//   console.log(i);
+// }
+
+for(let i = 0 ; i < 5 ; i++){
+  if(i===3){
+    continue;
+  }
+  console.log(i);
 }
-console.log(total)
