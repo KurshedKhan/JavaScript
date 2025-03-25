fc377abe6b6aab4e62a635124dc0dab9b8ef3402
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-03-25 11:43:52 +0530

    JavaScript Notes

---
+++ Day 12/index.js
@@ -0,0 +1,15 @@
+let student = {
+  name : "mohan",
+  age : 21,
+  height : 5.7,
+  roll : 101,
+  isResult : true,
+  marks : [23,65,87,54,78]
+}
+
+console.log(student);
+
+console.log(student.name);
+console.log(student["roll"]);
+console.log(student.marks[student.marks.length -1])
+console.log(student.marks[1])
