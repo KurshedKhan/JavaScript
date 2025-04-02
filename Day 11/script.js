1e1913dc4747e782a03eb128d9d879d936eaee41
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-04-02 11:37:13 +0530

    JavaScript Classes

--- Day 11/script.js
+++ Day 11/script.js
@@ -7,21 +7,39 @@
 
 
 // let text = document.getElementsByClassName("text");
+// console.log(text)
 // console.log(text[0].innerText);
 // console.log(text[1].innerText);
 
 
 // let para = document.getElementsByTagName("p");
-// console.log(para[0].innerText,para[1].innerText);
+// console.log(para[0].innerText,para[1].innerText); 
 
 
 // let para1 = document.getElementsByTagName("p");
-// for(let item of para){
+// for(let item of para1){
 //   console.log(item.innerText);
 // }
 
-let para1 = document.getElementsByTagName("p");
+// let para1 = document.getElementsByTagName("p");
 
 // Array.from(para1).forEach(function(value,index,array){
 //   console.log(value);
 // })
+
+// let img = document.getElementById("images");
+// // img.src = "Computer-cartoon.jpg";
+// img.setAttribute("src","Computer-cartoon.jpg");
+// // img.alt = "Computer Science";
+// img.setAttribute("alt","Computer images");
+// img.height = "100";
+// img.style.border = "2px solid red";
+
+
+
+// console.log(img);
+
+
+let btn = document.getElementById("btn");
+console.log(btn);
+btn.classList.add("button");
