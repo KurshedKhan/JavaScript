3abed3ea8b96e0b2e780a01f92e68af6e2300d8f
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-04-16 11:14:14 +0530

    JavaScript Classes

--- Day 11/Project1/JavaScript/script.js
+++ Day 11/Project1/JavaScript/script.js
@@ -4,26 +4,47 @@ let taskList = document.getElementById("Task-list");
 
 let tasks = []; 
 
+let day = new Date();
+
 btn.addEventListener('click',addTask);
 
 function renderTask(){
   taskList.innerHTML = "";
 
-  tasks.forEach((value,index)=>{
+  tasks.forEach((obj,index)=>{
 
     let li = document.createElement("li");
-    li.textContent = value;
+
+    if(obj.isEditing){
+      let input = document.createElement("input");
+      input.type = "text";
+      input.value = obj.text1;
+      input.addEventListener("change",(e) => obj.text1 = e.target.value);
+      li.appendChild(input);
+    }
+    else{
+      li.textContent = obj.text1 + " " + day.toDateString();
+    }
     
 
     let actions = document.createElement("div");
     actions.className = "action";
 
     let editBtn = document.createElement("button");
-    editBtn.textContent = "Edit";
+    editBtn.textContent = obj.isEditing ? "Save" : "Edit";
+    editBtn.className = obj.isEditing ? "save" : "";
+    editBtn.addEventListener("click",function(){
+      obj.isEditing = !obj.isEditing;
+      renderTask()
+    })
     
 
     let deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click",function(){
+      tasks.splice(index,1);
+      renderTask();
+    })
 
     actions.appendChild(editBtn);
     actions.appendChild(deleteBtn);
@@ -33,15 +54,13 @@ function renderTask(){
 
 }
 
-
-
 function addTask(){
   text = inputText.value.trim();
   if(text === ""){
     alert("please enter some value.");
   }
   else{
-    tasks.push(text);
+    tasks.push({text1:text,isEditing:false});
     inputText.value = "";
     renderTask();
   }
