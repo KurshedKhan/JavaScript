a56e0a44398b88775b1e98f17c677629aac38f46
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-06-13 08:05:42 +0530

    JavaScript classes one file

---
+++ script.js
@@ -0,0 +1,660 @@
+// // let roll = 102;
+// // let name = "mohit";
+// // let age = 21;
+
+// // console.log(roll);
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("Hello World!");
+// // console.log("---------------------------")
+// // let greet = "Good Morning,";
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+// // console.log(greet);
+
+
+// let Name = "mohit";
+// console.log("My Name is ",Name);
+// let firstName = "Mamta"; // reassignment operation
+// console.log("My Name is : ",Name);
+
+// const SCHOOL_NAME = "TECH INNOVATION";
+// console.log(SCHOOL_NAME);
+
+
+// // data type 
+
+// let name = "mohit"; // string type value 
+// let roll = 101; // number => integer value 
+// let price = 99.99; // number ==> floating point value
+// let isActive = true; // boolean value // condition statement
+// let isOff = false; // boolean value // condition statement
+// let age = undefined; // undefined type value
+// let result = undefined; // undefined type value
+// let odd = null; // absence of value
+// console.log(age);
+// console.log(result)
+// console.log(odd)
+
+// let num1 = 10;
+// let num2 = 20;
+
+// // Arthemetic Operation
+// console.log(num1+num2);
+// console.log(num2-num1);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+// console.log(num2%num1);
+
+// let firstNum = 2;
+// let secondNum = 3;
+
+// console.log(firstNum**secondNum);
+
+
+// // assignment operator
+
+// let name = "Mohan";
+// console.log(name);
+
+
+// let number = 10;
+
+// number+=30;
+// console.log(number);
+// number-=2;
+// console.log(number)
+// number*=3;
+// console.log(number);
+
+// // comparision // relational operator  return ==> true/false ==> boolean
+
+// console.log(10>=10);
+// console.log(10==20);
+// console.log(10>20);
+// console.log(10<20);
+// console.log(10>=20);
+// console.log(10<=20);
+
+// let age = 17;
+// console.log(age>18);
+
+// console.log(10!=10);
+
+// console.log(10=="10");
+// console.log(10==="10")
+// console.log(10 !== "20");
+
+// // logical Operator ==> two relational Operator use in single statement
+
+// let x = 10;
+// let y = 20;
+// console.log((x>y)||(x==y));
+
+
+// let age = 21;
+
+// if(age>=18){
+//   console.log("You are eligible for vote.");
+// }
+// else{
+//   console.log("You are not eligible for vote.");
+// }
+
+// let PenColor = "Purple";
+
+// if(PenColor === "Red"){
+//   console.log("This is my pen.");
+// }
+// else if(PenColor === "Green"){
+//   console.log("This is not my pen.");
+// }
+// else{
+//   console.log("This is not my pen bcz color differ.");
+// }
+
+// let trafficLight = "Yellow1";
+
+// if(trafficLight === "Red"){
+//   console.log("Stop.");
+// }
+// else if(trafficLight === "Yellow"){
+//   console.log("You can go but carefully.");
+// }
+// else if(trafficLight === "Green"){
+//   console.log("You can go now.");
+// }
+// else{
+//   console.log("Invalid data and please call the traffic police.");
+// }
+
+// let text = "mohan";
+
+// if(text === ""){
+//   console.log("Please Enter your value.")
+// }
+// else{
+//   console.log("This is value : ",text)
+// }
+
+// //  Check if a Number is Positive, Negative, or Zero
+// let num = 50;
+
+// if(num > 0){
+//   console.log("This is positive number",num);
+// }
+// else if(num < 0){
+//   console.log("This is negative number.",num);
+// }
+// else if(num === 0){
+//   console.log("This number is zero",num);
+// }
+// else{
+//   console.log("Invalid data");
+// }
+
+// find the largest two numbers
+
+// let num1 = 10;
+// let num2 = 20;
+
+// if(num1>num2){
+//   console.log("Num1 is largest then num2");
+// }
+// else if(num2>num1){
+//   console.log("Num2 is largest then num1");
+// }else{
+//   console.log("Invalid data");
+// }
+
+// let d = document.getElementById("demo");
+// d.innerText = "sohan";
+
+// let input = document.getElementById("input-text");
+// input.value = 45;
+// let text = document.getElementById("text");
+// text.innerText = "Tech Innovation";
+
+// let heading = document.getElementById("heading");
+// console.log(heading);
+// console.log(heading.innerHTML)
+// console.log(heading.innerText)
+
+// let day = "Mon";
+
+// switch(day){
+//   case "Sun":
+//     console.log("Today is Sun");
+//     break;
+//   case "Mon":
+//     console.log("Today is Mon");
+//     break;
+//   case "Tue":
+//     console.log("Today is Tue");
+//     break;
+//   case "Wed":
+//     console.log("Today is Wed");
+//     break;
+//   case "Thu":
+//     console.log("Today is Thu");
+//     break;
+//   case "Fri":
+//     console.log("Today is Fri");
+//     break;
+//   case "Sat":
+//     console.log("Today is Sat");
+//     break;
+//   default:
+//     console.log("This is invalid data.");
+//     break;
+// }
+
+
+// let fruitName = "carro";
+
+// switch(fruitName){
+//   case "Banana":
+//   case "Mango":
+//     console.log("This is fruits");
+//     break;
+//   case "carrot":
+//     console.log("This is vegetable");
+//     break;
+//   default:
+//     console.log("This data is invalid.");
+//     break;
+// }
+
+
+// let grade = "A";
+// switch (grade) {
+//     case "A":
+//         console.log("Excellent");
+//     case "B":
+//         console.log("Good");
+//     case "C":
+//         console.log("Average");
+//         break;
+//     default:
+//         console.log("Invalid grade");
+// }
+
+// null == false // output ==> false
+// undefined == false // output ==> false
+// 0 == false // output ==> true
+// 1 == true // output ==> true
+// null === undefined // output ==> false
+// null == undefined // output ==> true
+
+// null || undefined // output ==> null ==> false => k,a
+// undefined && null // output ==> undefined
+
+// for(let i = 1 ; i <= 5 ; i++){
+//   let age = prompt("Enter your age : ");
+//   if(age > 18){
+//     console.log("You are eligible for vote.",age);
+//   }
+//   else{
+//     console.log("You are not eligible for vote.",age);
+//   }
+// }
+
+
+// for(let i = 0 ; i > 5 ; i--){
+//   console.log(i);
+// }
+
+
+// let age = parseInt(prompt("Enter your age : "));
+// console.log(age);
+// // console.log(typeof age);
+
+// let num1 = "3";
+// let num2 = "5";
+// console.log(num1 + num1 * num2) // 
+
+
+// for(let i = 0 ; i < 5 ; i++){
+
+//   for(let j = 0 ; j < 5 ; j++){
+    
+//     for(let k = 0 ; k < 5 ; k++){
+
+//       console.log(i*j*k);
+      
+//     }
+//   }
+
+// }
+
+// string template
+
+
+//  let str = "";
+
+// for(let i = 0 ; i < 5 ; i++){
+//   for(let j = 0 ; j < 5 ; j++){
+//    str += "*";
+//   }
+//   str += "\n";
+// }
+// console.log(str);
+
+// let str = "";
+// for(let i = 1 ; i < 5 ; i++){
+//   for(let j = 0 ; j < i ; j++){
+//     str += "*";
+//   }
+//   str += "\n";
+// }
+
+// console.log(str);
+
+
+// let str = "";
+// for(let i = 1 ; i < 5 ; i++){
+
+//   for(let j = 5 ; j > i ; j--){
+//     str += "*";
+//   }
+//   str += "\n";
+// }
+
+// console.log(str);
+
+// for(let i = 1 ; i <= 5 ; i++){
+//   let age = prompt("Enter your age : ");
+//   if(age > 18 && age < 80){
+//     console.log("You are eligible for vote.",age);
+//   }
+//   else if(age >= 80){
+//     alert("Ghar beth ke aaram karo ji aap.");
+//     continue;
+//   }
+//   else{
+//     console.log("You are not eligible for vote.",age);
+//   }
+// }
+
+
+// Array
+
+// let marks = [23,45,65,67,54,34];
+// console.log(marks);
+// console.log(marks[1])
+// console.log(marks.length)
+// console.log(marks[marks.length-1])
+
+// for(let i = 0 ; i < marks.length ; i++){
+//   console.log(marks[i]);
+// }
+
+// let salaries = [];
+
+// let size = parseInt(prompt("how many items : "));
+
+
+// // empty array ==> add new values ;
+// for(let i = 0 ; i < size ; i++){
+//   salaries[i] = parseInt(prompt("Add new item : "));
+// }
+
+// // print array items
+// for(let i = 0 ; i < salaries.length ; i++){
+//   console.log(salaries[i]);
+// }
+
+// // Add new item in our array in position ending
+// let salaries = [];
+// console.log(salaries)
+
+// salaries.push(50000);
+// console.log(salaries);
+
+
+// salaries.push(20000);
+// console.log(salaries);
+
+// salaries.push(78000);
+// console.log(salaries)
+
+
+// // remove from ending part of array
+// salaries.pop();
+// console.log(salaries)
+
+
+// // add new item in our array from starting
+// salaries.unshift(12000);
+// console.log(salaries);
+
+// salaries.shift();
+// console.log(salaries);
+
+// console.log(salaries[5]);
+
+// let arr = [34,56,67,78,65,45];
+// let newArr = [...arr]; // copy
+// console.log(newArr);
+
+// let [first] = arr;
+// console.log(first);
+
+// let [first1,second,third,four,...rest] = arr;
+// console.log(first1,second,third,four,rest)
+
+// let colors = ["red", "blue", "green", "yellow", "purple"];
+
+// let newColors = colors.slice(2,4);
+// console.log(newColors);
+
+// function kya hota hai
+
+// function defination part
+// function display(){
+//   console.log("Hello World!!!");
+//   console.log("Hello World!!!");
+//   console.log("Hello World!!!");
+//   console.log("Hello World!!!");
+//   console.log("Hello World!!!");
+//   console.log("Hello World!!!");
+// }
+
+
+function spr(){
+  console.log("======================");
+}
+
+// display(); // function calling part
+// spr();
+// spr();
+// display();
+// spr();
+// display();
+// spr();
+
+
+// function add(num1,num2){
+//   let total = 0;
+//   total = num1 + num2;
+//   console.log("Total is : ",total);
+// }
+
+// add(10,20);
+// add(24,54);
+// spr();
+// add(56,43);
+// add(56,54);
+
+// function multi(num1,num2){
+//   let multiply = 0;
+//   multiply = num1 * num2;
+//   console.log("multiply is : ",  multiply);
+// }
+
+// multi(56,76);
+// add(56,76);
+
+
+// function calculator(num1,opr,num2){
+
+//   let result = 0;
+
+//   if(opr == "+"){
+//     result = num1 + num2;
+//   }
+//   else if(opr == "-"){
+
+//     if(num1>num2){
+//       result = num1 - num2;
+//     }
+//     else if(num2>num1){
+//       result = num2 - num1;
+//     }
+//   }
+//   else if(opr == "*"){
+//     result = num1 * num2;
+//   }
+//   else if(opr == "/"){
+//     if(num2 == 0){
+//       result = "num2 is zero so not divided by zero."
+//     }
+//     else{
+//        result = num1/num2;
+//     }
+//   }
+//   else{
+//     result = "Invaid Value";
+//   }
+//   console.log("My Result is : ",result);
+// }
+
+// let arg1 = parseInt(prompt("Enter your first number : "));
+// let opr1 = prompt("Enter your oprator : ");
+// let arg2 = parseInt(prompt("Enter your second number : "));
+
+// calculator(arg1,opr1,arg2);
+
+// function calculator(num1,opr,num2){
+
+//   if(opr == "+"){
+//     return num1 + num2;
+//   }
+//   else if(opr == "-"){
+
+//     if(num1>num2){
+//       return num1 - num2;
+//     }
+//     else if(num2>num1){
+//       return num2 - num1;
+//     }
+//   }
+//   else if(opr == "*"){
+//     return num1 * num2;
+//   }
+//   else if(opr == "/"){
+//     if(num2 == 0){
+//       return "num2 is zero so not divided by zero."
+//     }
+//     else{
+//        return num1/num2;
+//     }
+//   }
+//   else{
+//     return "Invaid Value";
+//   }
+// }
+
+// let arg1 = parseInt(prompt("Enter your first number : "));
+// let opr1 = prompt("Enter your oprator : ");
+// let arg2 = parseInt(prompt("Enter your second number : "));
+
+// let result = calculator(arg1,opr1,arg2);
+// console.log("My Result is : ",result);
+
+// function getName(myName){
+//   return myName;
+// }
+
+// let myname = prompt("Enter your name.");
+
+// let name = getName(myname);
+// console.log("My Name is " + name);
+
+// expression function
+// let getName = function(myName){
+//   return myName;
+// }
+
+// let myname = prompt("Enter your name.");
+
+// let name = getName(myname);
+// console.log("My Name is " + name);
+
+// Arrow Function
+
+// let getName = (myName="guest",age=18)=>{
+//   return [myName,age];
+// }
+
+// let myname = prompt("Enter your name.");
+// let myage = prompt("Enter your age.");
+
+// let [name,age] = getName(myname,myage);
+// console.log("My Name is " + name  + " My age is : " + age);
+
+
+// while(true){
+//   let age = prompt("Enter your age ");
+//   console.log("My age is : ",age);
+// }
+
+// 
+
+
+
+// for(let count = 0;count < 5;count++){
+//  console.log("My couting is : ",count);
+// }
+// let count = 0;
+// do{
+//   console.log("Runs at least one time",count);
+//   count++;
+// }while(count < 5);
+
+
+
+// const fruits = ["Apple", "Banana", "Grape"];
+
+// for(let i = 0 ;i < fruits.length ; i++){
+  //   console.log(fruits[i]);
+  // }
+  
+
+// for(let item of fruits){
+//   console.log(item);
+// }
+
+// for(let item in fruits){
+//   console.log(fruits[item]);
+// }
+
+
+// for(let item in marks){
+  //   console.log(marks[item]+10)
+  // }
+
+let marks = [33,54,56,65,78,76];
+let oddMarks = [];
+let evenMarks = [];
+
+for(let item of marks){
+
+  if(item % 2 !== 0){
+    oddMarks.push(item);
+  }
+  else{
+    evenMarks.push(item);
+  }
+
+}
+
+console.log(oddMarks);
+console.log(evenMarks);
+
+const names = ["Aarav", "Vivaan", "Aditya", "Vihaan", "Arjun", "Sai", "Reyansh", "Krishna", "Ishaan", "Shaurya", "Anaya", "Diya", "Myra", "Aadhya", "Kiara", "Pari", "Meera", "Saanvi", "Navya", "Ira", "Raj", "Amit", "Rohan", "Kunal", "Manav", "Sneha", "Pooja", "Nisha", "Kriti", "Riya"
+];
+
+let FiveLetter = [];
+let FiveAboveLetter = [];
+
+for(let item of names){
+  if(item.length >5){
+    FiveAboveLetter.push(item);
+  }
+  else{
+    FiveLetter.push(item);
+  }
+
+}
+console.log(FiveLetter)
+console.log(FiveAboveLetter)
